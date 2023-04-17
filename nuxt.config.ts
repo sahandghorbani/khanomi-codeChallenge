febfdb43b7c51b5e543e8c89bd50cbdd6ef14c7a
Sahand <sahand575@gmail.com>
2023-04-17 10:24:54 +0330

    add : add utility libraries

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    modules: [
+        "@nuxtjs/tailwindcss",
+        [
+          "@pinia/nuxt",
+          {
+            autoImports: ["defineStore", "acceptHMRUpdate"],
+          },
+        ],
+      ],
+      app: {
+        head: {
+          title: "Khanoomi-codeChallenge",
+          meta: [{ name: "Khanoomi-codeChallenge", content: "code challenge" }],
+          link: [
+            {
+              rel: "stylesheet",
+              href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+            },
+          ],
+        },
+      },
 })
